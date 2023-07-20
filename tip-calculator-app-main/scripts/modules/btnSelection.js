08aefda9c13060dd46ad4920f3fd8fc4315265f2
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-07-20 17:51:29 +0330

    Add functionality for select tip buttons

---
+++ tip-calculator-app-main/scripts/modules/btnSelection.js
@@ -0,0 +1,16 @@
+const btnSelection = () => {
+  const selectTipBtns = document.querySelectorAll(".select-tip__btn");
+  // Events for buttons
+  selectTipBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      // remove the class from all of the buttons
+      selectTipBtns.forEach((b) => {
+        b.classList.remove("clicked-btn");
+      });
+      // add the class to a button
+      e.target.classList.add("clicked-btn");
+    });
+  });
+};
+
+export default btnSelection;
