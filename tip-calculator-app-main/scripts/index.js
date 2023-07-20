0a0dddf81ea4c8424e99236afa2104e29a386720
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-07-20 17:51:50 +0330

    Import modules

---
+++ tip-calculator-app-main/scripts/index.js
@@ -0,0 +1,10 @@
+import inputSelection from "./modules/inputSelection.js";
+import btnSelection from "./modules/btnSelection.js";
+
+// selection
+inputSelection();
+btnSelection();
+
+// Error
+
+// Automation
