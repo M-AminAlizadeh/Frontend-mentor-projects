2158286872b37750811850a673d43860dd8f0e0e
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-07-24 11:54:12 +0330

    Add calculation and display functionality

--- tip-calculator-app-main/scripts/index.js
+++ tip-calculator-app-main/scripts/index.js
@@ -1,10 +1,12 @@
 import inputSelection from "./modules/inputSelection.js";
 import btnSelection from "./modules/btnSelection.js";
+import automation from "./modules/automation.js";
 
 // selection
 inputSelection();
 btnSelection();
 
-// Error
-
 // Automation
+automation();
+
+// Error
