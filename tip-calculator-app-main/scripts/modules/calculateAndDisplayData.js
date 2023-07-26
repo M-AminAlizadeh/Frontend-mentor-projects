d4b4a33e94934ef1094ff5a6143cdd41126ec1f9
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-07-26 12:44:30 +0330

    Change the calculation trigger mode from click to change any input

--- tip-calculator-app-main/scripts/modules/calculateAndDisplayData.js
+++ tip-calculator-app-main/scripts/modules/calculateAndDisplayData.js
@@ -1,74 +1,88 @@
 const calculateAndDisplayData = () => {
-  const billInput = document.querySelector('.bill__input');
+  const billInput = document.querySelector(".bill__input");
   const numberOfPeopleInput = document.querySelector(
-    '.number-of-people__input',
+    ".number-of-people__input"
   );
-  const selectTipBtns = document.querySelectorAll('.select-tip__btn');
-  const selectTipInput = document.querySelector('.select-tip__input');
+  const selectTipBtns = document.querySelectorAll(".select-tip__btn");
+  const selectTipInput = document.querySelector(".select-tip__input");
   const tipAmountPerPerson = document.querySelector(
-    '.show-screen__tip-amount__value',
+    ".show-screen__tip-amount__value"
   );
-  const totalPerPerson = document.querySelector('.show-screen__total__value');
-  const resetBtn = document.querySelector('.show-screen__reset-btn');
+  const totalPerPerson = document.querySelector(".show-screen__total__value");
+  const resetBtn = document.querySelector(".show-screen__reset-btn");
+  const inputs = [billInput, selectTipInput, numberOfPeopleInput];
   let billValue = 0;
   let tipValue = 0;
   let numberOfPeople = 0;
 
   // Collect info from user
-  billInput.addEventListener('input', (e) => {
+  billInput.addEventListener("input", (e) => {
     billValue = Number(e.target.value);
-    resetBtn.classList.remove('clicked-reset-btn');
+    resetBtn.classList.remove("clicked-reset-btn");
   });
 
   selectTipBtns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
+    btn.addEventListener("click", (e) => {
       tipValue = Number(e.target.value);
-      resetBtn.classList.remove('clicked-reset-btn');
+      resetBtn.classList.remove("clicked-reset-btn");
       // clear selectTip input
-      selectTipInput.value = '';
+      selectTipInput.value = "";
     });
   });
 
-  selectTipInput.addEventListener('input', (e) => {
+  selectTipInput.addEventListener("input", (e) => {
     // change style of btns
     selectTipBtns.forEach((btn) => {
-      btn.classList.remove('clicked-btn');
+      btn.classList.remove("clicked-btn");
     });
     tipValue = Number(e.target.value);
   });
 
-  numberOfPeopleInput.addEventListener('input', (e) => {
+  numberOfPeopleInput.addEventListener("input", (e) => {
     numberOfPeople = Number(e.target.value);
-    resetBtn.classList.remove('clicked-reset-btn');
+    resetBtn.classList.remove("clicked-reset-btn");
   });
 
   // Calculate & display results
-  document.addEventListener('click', () => {
-    if (billValue && numberOfPeople) {
+  const calculate = () => {
+    if (billValue && numberOfPeople && tipValue) {
+      const tipPercentage = tipValue / 100;
       tipAmountPerPerson.innerText = (
-        (billValue * (tipValue / 100))
-        / numberOfPeople
+        (billValue * tipPercentage) /
+        numberOfPeople
       ).toFixed(2);
       totalPerPerson.innerText = (
-        (billValue + billValue * (tipValue / 100))
-        / numberOfPeople
+        (billValue + billValue * tipPercentage) /
+        numberOfPeople
       ).toFixed(2);
     }
+  };
+
+  inputs.forEach((input) => {
+    input.addEventListener("input", () => {
+      calculate();
+    });
+  });
+
+  selectTipBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      calculate();
+    });
   });
 
   // Reset btn functionality
-  resetBtn.addEventListener('click', (e) => {
-    e.target.classList.add('clicked-reset-btn');
-    billInput.value = '';
-    selectTipInput.value = '';
+  resetBtn.addEventListener("click", (e) => {
+    e.target.classList.add("clicked-reset-btn");
+    billInput.value = "";
+    selectTipInput.value = "";
 
     selectTipBtns.forEach((btn) => {
-      btn.classList.remove('clicked-btn');
+      btn.classList.remove("clicked-btn");
     });
 
-    numberOfPeopleInput.value = '';
-    tipAmountPerPerson.innerText = '$0.00';
-    totalPerPerson.innerText = '$0.00';
+    numberOfPeopleInput.value = "";
+    tipAmountPerPerson.innerText = "$0.00";
+    totalPerPerson.innerText = "$0.00";
     billValue = 0;
     tipValue = 0;
     numberOfPeople = 0;
